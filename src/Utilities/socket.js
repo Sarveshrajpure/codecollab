2dde7e10fc4d81da00b943a1011740d6155a4d4e
sarvesh rajpure <rajpuresarvesh@gmail.com>
2022-08-14 00:27:22 +0530

    changed domain in socket.io config, added domain name in whatsApp share link

--- src/Utilities/socket.js
+++ src/Utilities/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ["websocket"],
   };
-  return io("http://offdutyninjas.site:3002", options);
+  return io("https://offdutyninjas.site:3002", options);
 };
--- src/Utilities/whatsAppMsg.js
+++ src/Utilities/whatsAppMsg.js
@@ -3,7 +3,7 @@ const createWhatsAppLink = (username, roomId) => {
 
   const SharingLink = `https://wa.me/?text=Hi! _${username}_ has invited you to a *Code Collab*,
     %0a%0aEnter the room this Room ID- *${roomId}*,
-    %0a%0aOr click on the link- link/${roomId}`;
+    %0a%0aOr click on the link- https://offdutyninjas.site/editor/${roomId}`;
 
   window.open(SharingLink, "_blank");
 };
