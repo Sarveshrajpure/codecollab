bd33d93ef6dd0549a4071b00cd732c7a4ddc9dd7
sarvesh rajpure <rajpuresarvesh@gmail.com>
2022-08-13 23:49:53 +0530

    changed socket.io port

--- src/Utilities/socket.js
+++ src/Utilities/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ["websocket"],
   };
-  return io("http://localhost:3002", options);
+  return io("http://offdutyninjas.site:3002", options);
 };
