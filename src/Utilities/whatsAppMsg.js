46872c1921afe193ac6852099c123dae7ae89a79
sarvesh rajpure <rajpuresarvesh@gmail.com>
2023-07-08 09:51:53 +0530

    updated WhatsApp link

--- src/Utilities/whatsAppMsg.js
+++ src/Utilities/whatsAppMsg.js
@@ -1,8 +1,7 @@
 const createWhatsAppLink = (username, roomId) => {
-
   const SharingLink = `https://wa.me/?text=Hi! _${username}_ has invited you to a *Code Collab*,
     %0a%0aEnter the room this Room ID- *${roomId}*,
-    %0a%0aOr click on the link- https://offdutyninjas.site/editor/${roomId}/false`;
+    %0a%0aOr click on the link- http://codecollab.offdutyninjas.pro/editor/${roomId}/false`;
 
   window.open(SharingLink, "_blank");
 };
