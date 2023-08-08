56641a09329b98e3018dfd7467f61cd953ded7f6
sarvesh rajpure <rajpuresarvesh@gmail.com>
2023-08-08 09:22:46 +0530

    Updated WhatsApp url, added content type in axiosInstance

--- src/Utilities/axiosHelper.js
+++ src/Utilities/axiosHelper.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: `/api`,
+  baseURL: `${baseUrl}/api`,
   withCredentials: true,
 });
+axios.defaults.headers.post["Content-Type"] = "application/json";
